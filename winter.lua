55b1dfd4e6cd1964a7edd7e0d97bd15b1c5de3ee
Snowyzzzz1 <120816047+Snowyzzzz1@users.noreply.github.com>
2022-12-18 02:21:03 -0300

    Add files via upload

--- winter.lua
+++ winter.lua
@@ -306,8 +306,7 @@ function farmraid()
                     pcall(function()
                         if not workspace.Mobs:FindFirstChild('Hive Queen') then
                             if v.Humanoid.WalkToPoint == Vector3.new(0,0,0) then
-                                swing = true
-                                repeat
+                                swing = true                                
                                     game.Players.LocalPlayer.Character:WaitForChild('HumanoidRootPart').CFrame = v.HumanoidRootPart.CFrame + Vector3.new(0,0,7)
                                     lookAt(game.Players.LocalPlayer.Character, v.HumanoidRootPart)
                                     task.wait()
